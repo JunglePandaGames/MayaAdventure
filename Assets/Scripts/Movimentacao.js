181bccfe47b983803f017bd7cc833c19056f1eb1
EberthFelipe <eberthfelipe@gmail.com>
2017-02-27 00:52:17 -0400

    Player Controls with Camera (idle, run and jump)

--- Assets/Scripts/Movimentacao.js
+++ Assets/Scripts/Movimentacao.js
@@ -1,57 +1,95 @@
 ﻿#pragma strict
 
+//[Header("Movement")]
 var velocidadeFrente : float;
 var velocidadeCima : float;
 var velocidadeLado : float;
+private final var ROTATE_SCALE : float = 90;
+private final var turnSpeed : float = 200f;
 
+
+//[Header("Components")]
+var animator : Animator;
+var rigidBody : Rigidbody;
+
+//[Header("Animation")]
+var isRunning : boolean = false;
+var isJumping : boolean = false;
 
 function Start () {
-
+    animator = GetComponent("Animator");
+    rigidBody = GetComponent("Rigidbody");
 }
 
 function Update () {
-
-velocidadeCima = 15*Time.deltaTime;
-velocidadeFrente = 10*Time.deltaTime;
-velocidadeLado = 10*Time.deltaTime;
-
-if(Input.GetKey("w"))
-
-{
-
-transform.Translate(0,0,velocidadeFrente);
-
-	}
-
-if(Input.GetKey("s"))
-
-{
-
-transform.Translate(0,0,-velocidadeFrente);
-
-	}
-
-if(Input.GetKey("a"))
-
-{
-
-transform.Translate(-velocidadeLado,0,0);
-
-	}
-
-if(Input.GetKey("d"))
-
-{
-
-transform.Translate(velocidadeLado,0,0);
-
-	}
-
-if(Input.GetKey("space"))
-
-{
-
-transform.Translate(0,velocidadeCima,0);
-
-	}
+    velocidadeCima = 15*Time.deltaTime;
+    velocidadeFrente = 10*Time.deltaTime;
+    velocidadeLado = 10*Time.deltaTime;
+    //input
+    MovementsController();
+    //animation / input
+    AnimationController();
+    //UI update set animation
+    UpdateAnimetionParameters();
+    
 }
+function MovementsController(){
+    //mover para frente
+    if(Input.GetKey("w")){
+        transform.Translate(0,0,velocidadeFrente);
+        //transform.Rotate = new Vector3(0,ROTATE_SCALE*0,0);
+        
+    }
+
+    //mover para trás
+    if(Input.GetKey("s")){
+        transform.Translate(0,0,-velocidadeFrente);
+        //transform.Rotate = new Vector3(0,ROTATE_SCALE*2,0);
+    }
+
+    //mover para esquerda
+    if(Input.GetKey("a")){
+        transform.Translate(-velocidadeLado,0,0);
+        //transform.Rotate(Vector3(0,ROTATE_SCALE*1,0));
+        transform.Rotate(Vector3.up , -turnSpeed * Time.deltaTime);
+        
+    }
+
+    //mover para direita
+    if(Input.GetKey("d")){
+        transform.Translate(velocidadeLado,0,0);
+        //transform.Rotate((0,ROTATE_SCALE*3,0));
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        
+    }
+
+    if(Input.GetKey("space")){
+        transform.Translate(0,velocidadeCima,0);
+    }
+}
+function AnimationController(){
+    var moveHorizontal = Input.GetAxis("Horizontal");
+    var moveVertical = Input.GetAxis("Vertical");
+    var moveJump = Input.GetAxis("Jump");
+
+    //movimento lateral e/ou movimento vertical (frente e trás)
+    if(moveHorizontal == 0 && moveVertical == 0){
+        isRunning = false;
+    } else {
+        isRunning = true;
+    }
+    
+    //movimento Jump
+    if(moveJump == 0){
+        isJumping = false;
+    } else {
+        isJumping = true;
+    }
+    
+}
+
+function UpdateAnimetionParameters(){
+    animator.SetBool("isRunning", isRunning);
+    animator.SetBool("isJumping", isJumping);
+}
+
