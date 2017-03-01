1c8263822d84a256ec149e77ccde2b81c4920542
taymonchris <taymon.chris@gmail.com>
2017-03-01 12:40:06 -0400

    Movimento dos inimigos comuns

---
+++ Assets/EnemyBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyBehavior : MonoBehaviour {
+    Transform player;
+    NavMeshAgent nav;
+    
+	
+    // Use this for initialization
+	void Awake () {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        nav = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        nav.SetDestination(player.position);
+        Debug.Log(player.position);
+	}
+}
