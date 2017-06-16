43f108cdb100dd0d0026121c07691bb164fca65d
taymonchris <taymon.chris@gmail.com>
2017-06-16 12:03:50 -0400

    Organização dos assets

---
+++ Assets/AssetsUsadoNoMenu/Scripts/CenterPosition.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+var target : Transform;
+// ROTATE CAMERA:
+var rotation:Quaternion;
+
+function Start () {
+    // ROTATE CAMERA:
+    var rotation = Quaternion.Euler(0, 0, 0);
+}
+
+function Update () {
+    // transform.position = target.position;
+    transform.position = new Vector3(target.position.x, transform.position.y,target.position.z);
+    
+}
---
+++ Assets/Scripts/RestartButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartButton : MonoBehaviour {
+	//GameObject p;
+
+	void Start(){
+	//	p = GameObject.FindGameObjectWithTag ("Player").GetComponent<> ();
+	}
+	public void RestartScene(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+
+}
